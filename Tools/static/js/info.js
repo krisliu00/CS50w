df8039be33a2b3fc3ad8f34630de9d449785bf13
krisliu00 <oydiss@gmail.com>
2024-05-11 22:45:42 +0800

    tools updated

---
+++ Tools/static/js/info.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  function fetchMarkdown(url, textContent) {
+    return fetch(url)
+        .then(response => response.text())
+        .then(markdown => convertMarkdownToHtml(markdown, textContent))
+        .catch(error => console.error('Error fetching Markdown:', error));
+  }
+
+  function convertMarkdownToHtml(markdownText, textContent) {
+      var converter = new showdown.Converter();
+      var html = converter.makeHtml(markdownText);
+      document.getElementById('textarea').innerHTML = html;
+      document.getElementById('title').innerHTML = textContent;
+      console.log('Dropdown item text content:', textContent); 
+  }
+
+  document.querySelectorAll('#links-container .dropdown-item').forEach(link => {
+      link.addEventListener('click', function(event) {
+          event.preventDefault(); 
+          const fileUrl = this.getAttribute('data-url');
+          const textContent = this.textContent.trim(); 
+
+          fetchMarkdown(fileUrl, textContent);
+      });
+  });
+
+});
