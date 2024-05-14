de041b4087a8ab1a9aff137fbb2d5784f83666ff
krisliu00 <oydiss@gmail.com>
2024-05-14 22:26:07 +0800

    lates update

--- Tools/md/Python.md
+++ Tools/md/Python.md
@@ -1,4 +1,5 @@
 - 引用数据库数据时，`get_object_or_404（）`返回的是一个单一的模型实例， 在template里可以直接用.field来引用，比如`{{ auction.image_url }}`
+
 - `AuctionList.objects.filter()`返回的是QuerySet，包含所有符合条件的实例，即使只有一个实例，也需要用遍历来引用，比如
     ```
     {% for obj in auctions %}
--- Tools/static/js/info.js
+++ Tools/static/js/info.js
@@ -1,35 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     daily();
-
-        document.querySelector('#calculatorarea').style.display = 'none';
-        document.querySelector('#displayarea').style.display = 'block';
         
+    const copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/></svg>';
+    const copiedIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
 
-  
-      const copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/></svg>';
-      const copiedIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
-
-      const copyIconElement = document.createElement('div');
-      copyIconElement.innerHTML = copyIcon;
-      const copiedIconElement = document.createElement('div');
-      copiedIconElement.innerHTML = copiedIcon;
-
-      copyIconElement.classList.add('copy-docs-icon');
-      copiedIconElement.classList.add('docs-copied-icon');
-
-      document.body.appendChild(copyIconElement);
-      document.body.appendChild(copiedIconElement);
-
-      $(document).ready(function() {
-        $('#calculator').click(function() {
-            $("#calculatorarea, #displayarea").toggleClass("hidden");
+    $(document).ready(function() {
+    $('#calculator').click(function() {
+        $("#calculatorarea, #displayarea").toggleClass("hidden");
         });
     });
 
       
-
-
-
   function fetchMarkdown(url, textContent) {
       return fetch(url)
           .then(response => response.text())
